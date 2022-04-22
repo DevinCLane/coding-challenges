9e720a5c75705683280fb80b5f30e99aee3ecdba
Devin Lane <devin.c.lane@gmail.com>
2022-04-22 12:52:43 -0700

    coding challenge for today

---
+++ 2022-04-22/app.js
@@ -0,0 +1,18 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+// Example : n=5 --> [5,4,3,2,1]
+
+// My notes:
+// first, we create an empty array to store our result
+// next, we want to go from n to 0, so we set our counter variable i to n (which is 5 in the test case)
+// we then push i to the array and decrement i each time until i = 1 (in other words, run the loop as long as i > 0)
+// we return the new array
+
+const reverseSeq = n => {
+    let array = [];
+    for (i = n; i > 0; i--) {
+        array.push(i);
+    }
+    return array;
+};
+
+console.log(reverseSeq(5), [5, 4, 3, 2, 1]);
