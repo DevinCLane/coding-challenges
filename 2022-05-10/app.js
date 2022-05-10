60db6b08ed763f67062f11849e4d5b785c851ad8
Devin Lane <devin.c.lane@gmail.com>
2022-05-10 12:18:02 -0700

    how good are you really?

---
+++ 2022-05-10/app.js
@@ -0,0 +1,32 @@
+/* 
+https://www.codewars.com/kata/5601409514fc93442500010b
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+Note:
+
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+*/
+
+function betterThanAverage(classPoints, yourPoints) {
+    classPoints.push(yourPoints);
+    classPoints.sort((a, b) => a - b);
+    let sum = 0;
+    for (let i = 0; i < classPoints.length; i++) {
+        sum += classPoints[i];
+    }
+    let average = sum / classPoints.length;
+    if (yourPoints > average) {
+        return true;
+    } else return false;
+}
+
+
+console.log(betterThanAverage([2, 3], 5), true);
+console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75), true);
+console.log(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9), false);
+console.log(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50), false);
+console.log(betterThanAverage([29, 55, 74, 60, 11, 90, 67, 28], 21), false);
