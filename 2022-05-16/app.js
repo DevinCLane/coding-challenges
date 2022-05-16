d93a1dcd5445eaa5368e6434f7d4bb379285d6f0
Devin Lane <devin.c.lane@gmail.com>
2022-05-16 08:59:50 -0700

    playing with digits

---
+++ 2022-05-16/app.js
@@ -0,0 +1,50 @@
+/* 
+https://www.codewars.com/kata/5552101f47fc5178b1000050/train/javascript
+Some numbers have funny properties. For example:
+
+    89 --> 8¹ + 9² = 89 * 1
+
+    695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+    46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
+
+    we want to find a positive integer k, if it exists, such that the sum of the digits of n taken to the successive powers of p is equal to k * n.
+
+In other words:
+
+    Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+
+If it is the case we will return k, if not return -1.
+
+Note: n and p will always be given as strictly positive integers.
+
+digPow(89, 1) should return 1 since 8¹ + 9² = 89 = 89 * 1
+digPow(92, 1) should return -1 since there is no k such as 9¹ + 2² equals 92 * k
+digPow(695, 2) should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
+digPow(46288, 3) should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+*/
+
+/* 
+PREP:
+Parameters: Number, then power that we begin at. always positive, always numbers.
+Return: K if it works, -1 if not
+Example: 
+digPow(92, 1) should return -1 since there is no k such as 9¹ + 2² equals 92 * k
+digPow(695, 2) should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+I was 80% of the way there, then peeked at the solution to take me to the finish line.
+*/
+
+function digPow(n, p) {
+    // split n by each digit, take that digit to the successive powers of p, add them up
+    let sum = String(n).split('').reduce((sum, value, power) => sum + Math.pow(value, p + power), 0);
+    return sum % n ? -1 : sum / n // if sum % n exists, that means it's not 0, in other words there's a remainder (doesn't fit our requirements);
+}
+
+// test cases
+console.log(digPow(89, 1), 1)
+console.log(digPow(92, 1), -1)
+console.log(digPow(46288, 3), 51)
