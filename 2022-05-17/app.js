c1fdab6a7ae97ac0c5a239e88dae1e8d1530706e
Devin Lane <devin.c.lane@gmail.com>
2022-05-17 09:17:54 -0700

    sum of numbers

---
+++ 2022-05-17/app.js
@@ -0,0 +1,45 @@
+/* 
+https://www.codewars.com/kata/55f2b110f61eb01779000053/train/javascript
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+Examples (a, b) --> output (explanation)
+
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+*/
+
+/* 
+PREP
+Parameters: two integers, negative or positive, always integers.
+Returns: one number, the sum of integers between them inclusive. if the two are equal, return one of them
+Example: -2, 3: -2 + -1 + 0 + 1 + 2 + 3 = 3
+*/
+
+const getSum = (a, b) => {
+    // if a === b, return a;
+    let sum = 0;
+    if (a === b) {
+        return a;
+    } else if (a < b) {
+        while (a <= b) {
+            sum += a
+            a++
+        }
+    } else if (a > b) {
+        while (a >= b) {
+            sum += a;
+            a--
+        }
+    } return sum
+    // if a < b loop starting at a, count each integer while <= b
+    // if a > b loop starting at b, count each integer down while b >= a
+}
+
+// test cases
+console.log(getSum(0,-1),-1);
+console.log(getSum(0,1),1);
