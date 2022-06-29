f8715bc756bb8905b3a05418e4814b412a490f2f
Devin Lane <devin.c.lane@gmail.com>
2022-06-29 12:42:01 -0700

    messi goals

---
+++ 2022-06-29/app.js
@@ -0,0 +1,32 @@
+/* 
+Messi's Goal Total
+
+Use variables to find the sum of the goals Messi scored in 3 competitions
+Information
+
+Messi goal scoring statistics:
+Competition 	Goals
+La Liga 	43
+Champions League 	10
+Copa del Rey 	5
+Task
+
+    Create these three variables and store the appropriate values using the table above:
+
+    laLigaGoals
+    championsLeagueGoals
+    copaDelReyGoals
+
+    Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+
+*/
+
+const laLigaGoals = 43;
+const championsLeagueGoals = 10;
+const copaDelReyGoals = 5;
+
+let totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+
+// test case
+console.log(totalGoals, 58)
