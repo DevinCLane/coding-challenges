655f61ef6681325a42f94b29042f60887d57910b
Devin Lane <devin.c.lane@gmail.com>
2022-11-10 09:40:24 -0800

    array diff

---
+++ 2022-11-10/array-diff.js
@@ -0,0 +1,30 @@
+/* 
+Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b keeping their order.
+
+arrayDiff([1,2],[1]) == [2]
+
+If a value is present in b, all of its occurrences must be removed from the other:
+
+arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+
+arrayDiff([], [4,5]), [], "a was [], b was [4,5]"
+
+arrayDiff([3,4], [3]), [4], "a was [3,4], b was [3]"
+
+arrayDiff([1,8,2], []), [1,8,2], "a was [1,8,2], b was []"
+*/
+
+function arrayDiff(a, b) {
+    // make a new Set from b, to use Set methods which are faster than some array methods
+    const set = new Set(b)
+    // does the number in a exist in b? if it does not, then we pass it along. if it does, then we don't pass it along.
+    return a.filter(n => !set.has(n))
+}
+
+console.log(arrayDiff([1,2,2,2,3],[2]), [1,3])
+console.log(arrayDiff([], [4,5]), [], "a was [], b was [4,5]")
+console.log(arrayDiff([3,4], [3]), [4], "a was [3,4], b was [3]")
+console.log(arrayDiff([1,8,2], []), [1,8,2], "a was [1,8,2], b was []")
