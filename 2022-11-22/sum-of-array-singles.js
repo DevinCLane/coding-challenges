22f5f6ec2e1a89f0d8df79f42bfaed72432cc67e
Devin Lane <devin.c.lane@gmail.com>
2022-11-22 11:50:33 -0800

    sum of array singles

---
+++ 2022-11-22/sum-of-array-singles.js
@@ -0,0 +1,24 @@
+/* 
+You will be given an array of numbers in which two numbers occur once and the rest occur only twice. 
+Your task will be to return the sum of the numbers that occur only once. 
+For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. 
+Every other number occurs twice.
+
+repeats([4,5,7,5,4,8]),15)
+repeats([9, 10, 19, 13, 19, 13]),19)
+repeats([16, 0, 11, 4, 8, 16, 0, 11]),12)
+repeats([5, 17, 18, 11, 13, 18, 11, 13]),22)
+repeats([5, 10, 19, 13, 10, 13]),24)
+*/
+
+const repeats = arr => {
+    // find the numbers that occur once
+    // add them up
+    return arr.filter((e, i, arr) => arr.indexOf(e) === arr.lastIndexOf(e)).reduce((a, c) => a + c, 0)
+}
+
+console.log(repeats([4,5,7,5,4,8]),15)
+console.log(repeats([9, 10, 19, 13, 19, 13]),19)
+console.log(repeats([16, 0, 11, 4, 8, 16, 0, 11]),12)
+console.log(repeats([5, 17, 18, 11, 13, 18, 11, 13]),22)
+console.log(repeats([5, 10, 19, 13, 10, 13]),24)
