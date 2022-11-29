c052ab7374404cf7b728222a3aa3025029cb8009
Devin Lane <devin.c.lane@gmail.com>
2022-11-29 12:09:07 -0800

    string array duplicates

---
+++ 2022-11-29/string-array-duplicates.js
@@ -0,0 +1,27 @@
+/* 
+You will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+For example:
+
+dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+dup(["kelless","keenness"]) = ["keles","kenes"].
+Strings will be lowercase only, no spaces 
+
+dup(["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"]),['codewars','picaniny','hubububo'])
+dup(["abracadabra","allottee","assessee"]),['abracadabra','alote','asese'])
+dup(["kelless","keenness"]), ['keles','kenes'])
+*/
+
+const dup = arr => {
+    const result = []
+    for (const item of arr) {
+        result.push(item.split('').filter((element, index, array) => element !== array[index - 1]).join(''))
+    }
+    return result
+    // reduce array, if prev === cur, remove current, otherwise move on
+
+}
+
+console.log(dup(["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"]),['codewars','picaniny','hubububo'])
+console.log(dup(["abracadabra","allottee","assessee"]),['abracadabra','alote','asese'])
+console.log(dup(["kelless","keenness"]), ['keles','kenes'])
