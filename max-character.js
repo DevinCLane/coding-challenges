a7ca8e785ed6628c88140e9602f07c25d9c02abf
Devin Lane <devin.c.lane@gmail.com>
2022-12-12 12:44:21 -0800

    max character update comments

--- max-character.js
+++ max-character.js
@@ -25,7 +25,7 @@ const maxCharacter = str => {
         }
     }
     // use object.keys to get an array of our key names (['h', 'e', 'l'...etc])
-    // then use reduce to look at each key and ask, is this number bigger than the current? if so return the acc, otherwise return the curr key
+    // then use reduce to look at each key and ask, is this number bigger than the current? if so return the acc key name (the letter), otherwise return the curr key name
     return Object.keys(map).reduce((acc, curr) => map[acc] > map[curr] ? acc : curr, 0)
 }
 
@@ -36,27 +36,30 @@ console.log(maxCharacter("Hello World"), "l")
 
 const maxCharacter2 = str => {
     const charMap = {}
+    // this will store the highest value of letter occurance
     let count = 0,
+        // this will store the character associated with that highest value
         maxChar = null;
         
     // create the character map of the string
     for (const char of str) {
+        // interestingly here if you write charMap[char]++, it doesn't work. Need to write ++charMap[char]
         charMap[char] = charMap[char] + 1 || 1;
     }
 
     // use a for in loop to loop over iterable properties
-    // use count to store the highest count of numbers
     for (const char in charMap) {
+        // use count to store the highest count of numbers
+        // update count with the new character number if it's the biggest
         if (charMap[char] > count) {
-            // update count with the new character number if it's the biggest
             count = charMap[char]
             // update the maxChar with the newest biggest character
             maxChar = char
         }
+        // we don't do anything if the current letter in our map is lower than our current highest `count`
     }
+    // return the letter assocated with the highest count
     return maxChar
-        
-
 }
 
 console.log(maxCharacter2('Hello World'), 'l')
