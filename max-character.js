2c8a522762991dc69a685ddf55065c7fcc41b844
Devin Lane <devin.c.lane@gmail.com>
2022-12-12 12:41:06 -0800

    max character

--- max-character.js
+++ max-character.js
@@ -14,7 +14,9 @@ https://medium.com/siliconwat/algorithms-in-javascript-b0bed68f4038
 */
 
 const maxCharacter = str => {
+    // create a map to store our values
     const map = {}
+    // loop over the string and create our map of values counting letter occurances
     for (const letter of str.toLowerCase()) {
         if (map[letter]) {
             map[letter]++
@@ -22,8 +24,39 @@ const maxCharacter = str => {
             map[letter] = 1;
         }
     }
+    // use object.keys to get an array of our key names (['h', 'e', 'l'...etc])
+    // then use reduce to look at each key and ask, is this number bigger than the current? if so return the acc, otherwise return the curr key
     return Object.keys(map).reduce((acc, curr) => map[acc] > map[curr] ? acc : curr, 0)
 }
 
 console.log(maxCharacter("Hello World"), "l")
 
+
+// another approach
+
+const maxCharacter2 = str => {
+    const charMap = {}
+    let count = 0,
+        maxChar = null;
+        
+    // create the character map of the string
+    for (const char of str) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    // use a for in loop to loop over iterable properties
+    // use count to store the highest count of numbers
+    for (const char in charMap) {
+        if (charMap[char] > count) {
+            // update count with the new character number if it's the biggest
+            count = charMap[char]
+            // update the maxChar with the newest biggest character
+            maxChar = char
+        }
+    }
+    return maxChar
+        
+
+}
+
+console.log(maxCharacter2('Hello World'), 'l')
