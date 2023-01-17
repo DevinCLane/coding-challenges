edf77f3c235e2caa7c5f0109fb425ab37e58bd42
Devin Lane <devin.c.lane@gmail.com>
2023-01-17 09:43:10 -0800

    reverse string

---
+++ 2023-01-17/reverse-string.js
@@ -0,0 +1,24 @@
+/* 
+Reverse String
+
+Given a string of characters as input, write a function that returns it with the characters reversed.
+No Reverse Method (well brute force it first, but then no reverse method)!
+*/
+
+const reverseString = str => {
+    let result = ''
+    for (let i = str.length - 1; i >= 0; i--) {
+        result = result + str[i] 
+    }
+    return result;
+}
+
+console.log(reverseString('hello'), 'olleh')
+console.log(reverseString('goodbye'), 'eybdoog')
+
+const reverseString2 = str => {
+    return str.split('').reduce((acc, curr) => curr + acc, '')
+}
+
+console.log(reverseString2('hello'), 'olleh')
+console.log(reverseString2('goodbye'), 'eybdoog')
