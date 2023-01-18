eae4ac62e7deb288e06f09ce6b39164452844274
Devin Lane <devin.c.lane@gmail.com>
2023-01-18 10:27:21 -0800

    jewels and stones

---
+++ 2023-01-18/jewels-and-stones.js
@@ -0,0 +1,31 @@
+/* 
+Jewels and stones
+
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+ 
+Example 1:
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+ 
+Constraints:
+1 <= jewels.length, stones.length <= 50
+jewels and stones consist of only English letters.
+All the characters of jewels are unique.
+https://leetcode.com/problems/jewels-and-stones/
+*/
+
+const jewelsAndStones = (jewels, stones) => {
+    // loop through the jewels
+    // check which ones exist within stones
+    const jewelsSet = new Set(jewels)
+    // using a set here, more optimized than an array lookup
+    return stones.split('').reduce((acc, curr) => acc + jewelsSet.has(curr), 0)
+}
+
+console.log(jewelsAndStones('aA', 'aAAbbbb'), 3)
+console.log(jewelsAndStones('z', 'ZZ'), 0)
+console.log(jewelsAndStones('abC', 'eeeddddagggBbcCa'), 4)
