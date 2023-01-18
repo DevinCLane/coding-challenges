24bb1dd6cbdf9819238921c2e04439bbb82bb91e
Devin Lane <devin.c.lane@gmail.com>
2023-01-18 10:38:49 -0800

    count chars

---
+++ 2023-01-18/count-chars.js
@@ -0,0 +1,16 @@
+/* 
+Count characters in your string
+
+The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+What if the string is empty? Then the result should be empty object literal, {}.
+*/
+
+const countChars = str => {
+    const map = {}
+    for (const char of str) {
+        map[char] = map[char] + 1 || 1
+    }
+    return map
+}
+
+console.log(countChars('hello'), {'h': 1, 'e': 1, 'l': 2, 'o': 1})
