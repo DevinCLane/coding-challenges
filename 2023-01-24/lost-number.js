630ddc0f55d8c55fb4bbb277ff059d97abb1c34c
Devin Lane <devin.c.lane@gmail.com>
2023-01-24 09:18:29 -0800

    lost number 3 ways

--- 2023-01-24/lost-number.js
+++ 2023-01-24/lost-number.js
@@ -24,3 +24,30 @@ const lostNumber = (starting, mixed) => {
 console.log(lostNumber([1,2,3,4,5], [3,4,1,5]), 2, 'Deletion') 
 console.log(lostNumber([1,2,3,4,5,6,7,8,9], [1,9,7,4,6,2,3,8]), 5, 'Deletion') 
 console.log(lostNumber([1,2,3,4,5,6,7,8,9], [5,7,6,9,4,8,1,2,3]), 0, 'No deletion')
+
+// we could also subtract the sum of the two arrays to get the difference
+
+const lostNumber2 = (starting, mixed) => {
+    if (starting.length === mixed.length) {
+        return 0;
+    }
+    const startingSum = starting.reduce((acc, curr) => acc + curr, 0)
+    const mixedSum = mixed.reduce((acc, curr) => acc + curr, 0)
+    return startingSum - mixedSum
+}
+
+console.log(lostNumber2([1,2,3,4,5], [3,4,1,5]), 2, 'Deletion') 
+console.log(lostNumber2([1,2,3,4,5,6,7,8,9], [1,9,7,4,6,2,3,8]), 5, 'Deletion') 
+console.log(lostNumber2([1,2,3,4,5,6,7,8,9], [5,7,6,9,4,8,1,2,3]), 0, 'No deletion')
+
+// for loops / naive approach
+// sort the array, walk through and ask if the values are the same
+const lostNumber3 = (starting, mixed) => {
+    mixed.sort((a, b) => a - b)
+    for (let i = 0; i < starting.length; i++) {
+        if (starting[i] !== mixed[i]) {
+            return starting[i]
+        }
+    }
+    return 0
+}
