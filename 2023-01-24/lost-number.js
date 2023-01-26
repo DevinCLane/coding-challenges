4cee0402d9b0657d4f63cda023d398babd32b8fb
Devin Lane <devin.c.lane@gmail.com>
2023-01-26 08:50:59 -0800

    lost numbers test cases

--- 2023-01-24/lost-number.js
+++ 2023-01-24/lost-number.js
@@ -51,3 +51,7 @@ const lostNumber3 = (starting, mixed) => {
     }
     return 0
 }
+
+console.log(lostNumber3([1,2,3,4,5], [3,4,1,5]), 2, 'Deletion') 
+console.log(lostNumber3([1,2,3,4,5,6,7,8,9], [1,9,7,4,6,2,3,8]), 5, 'Deletion') 
+console.log(lostNumber3([1,2,3,4,5,6,7,8,9], [5,7,6,9,4,8,1,2,3]), 0, 'No deletion')
