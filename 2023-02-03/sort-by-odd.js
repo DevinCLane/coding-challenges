a745be097eaa0469cdaf774e86654a6810ce077f
Devin Lane <devin.c.lane@gmail.com>
2023-02-03 10:13:31 -0800

    sort by odds

---
+++ 2023-02-03/sort-by-odd.js
@@ -0,0 +1,22 @@
+/* 
+Sort by odd
+Task
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+https://www.codewars.com/kata/578aa45ee9fd15ff4600090d
+*/
+
+const sortByOdd = arr => {
+    // find the odds, put them in an array
+    // map over the original array, if it's even, pass it through, it it's odd, pull from the odds array
+    // sort the odds in descending order so that we can use pop instead of shift because it's more efficient.
+    const odds = arr.filter(value => value % 2 !== 0);
+    return arr.map(value => value % 2 === 0 ? value : odds.pop())
+}
+
+console.log(sortByOdd([7, 1]), [1, 7])
+console.log(sortByOdd([5, 8, 6, 3, 4]), [3, 8, 6, 5, 4])
+console.log(sortByOdd([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]), [1, 8, 3, 6, 5, 4, 7, 2, 9, 0])
