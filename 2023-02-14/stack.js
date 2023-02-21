0cf4b728bce9c94459b3c605a927ae292f42347f
Devin Lane <devin.c.lane@gmail.com>
2023-02-21 09:08:15 -0800

    stack

---
+++ 2023-02-14/stack.js
@@ -0,0 +1,14 @@
+/* 
+Implement a stack in JavaScript
+
+a stack is a FILO data structure. Picture a stack of plates. As you add plates they accumulate on the top. When you grab a plate you grab the plate from the top.
+
+we will implement push -- add something to the top of the stack
+pop -- take something off the top of the stack
+peek -- 
+*/
+
+
+class Stack {
+
+}
