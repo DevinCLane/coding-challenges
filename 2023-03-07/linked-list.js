35af5b3864e41a20e88dc9c8c4fe0a1c074db84d
Devin Lane <devin.c.lane@gmail.com>
2023-03-07 09:06:48 -0800

    linked list

---
+++ 2023-03-07/linked-list.js
@@ -0,0 +1,129 @@
+/* 
+Implement a doubly linked list in JavaScript
+
+Append
+Prepend
+deleteHead
+deleteTail
+search
+
+node
+*/
+
+class LinkedList {
+    constructor() {
+        this.head = this.tail = null
+    }
+
+    append(value) {
+        // if the list is empty
+        if (!this.tail) {
+            this.tail = this.head = new Node(value)
+        }
+        else {
+            let oldTail = this.tail
+            this.tail = new Node(value)
+            this.tail.prev = oldTail
+            oldTail.next = this.tail
+        }
+    }
+
+    prepend(value) {
+        // if the list is empty
+        if (!this.head) {
+            this.head = this.tail = new Node(value)
+        }
+        else {
+            let oldHead = this.head
+            this.head = new Node(value)
+            this.head.next = oldHead
+            oldHead.prev = this.head
+        }
+    }
+
+    deleteHead() {
+        // if the list is empty
+        if (!this.head) {
+            return null
+        }
+        else {
+            let removedHead = this.head
+            // if there is only 1 node left
+            if (this.head === this.head) {
+                this.head = this.tail = null
+            }
+            // if there is > 1 node left
+            else {
+                this.head = this.head.next
+                this.head.prev = null
+            }
+            return removedHead.value
+        }
+    }
+
+    deleteTail() {
+        // if the list is empty
+        if (!this.tail) {
+            return null
+        }
+        else{
+            let removedTail = this.tail
+            // if there is only 1 node left
+            if (this.tail === this.head) {
+                this.tail = this.head = null
+            }
+            // if there is > 1 node left
+            else {
+                this.tail = this.tail.prev
+                this.tail.next = null
+            }
+            return removedTail.value
+        }
+    }
+
+    search(value) {
+        let currentNode = this.head
+        while (currentNode) {
+            if (currentNode.value === value) {
+                return currentNode
+            }
+            currentNode = currentNode.next
+        }
+        return null
+    }
+}
+
+class Node {
+    constructor(value, prev, next) {
+        this.value = value
+        this.prev = prev || null
+        this.next = next || null
+    }
+}
+
+const linkedList = new LinkedList()
+console.log(linkedList)
+linkedList.append(1)
+console.log(linkedList)
+linkedList.append(2)
+console.log(linkedList)
+linkedList.append(3)
+console.log(linkedList)
+linkedList.append(4)
+console.log(linkedList)
+linkedList.append(5)
+console.log(linkedList)
+linkedList.prepend(0)
+console.log(linkedList)
+linkedList.prepend(-1)
+console.log(linkedList)
+linkedList.deleteHead() // 0 1 2 3 4 5
+console.log(linkedList, 'delete head')
+linkedList.deleteTail() // 1 2 3 4
+console.log(linkedList, 'delete tail')
+linkedList.search(4) // 1 2 3 4
+console.log(linkedList)
+linkedList.search(2) // 1 2 3 4
+console.log(linkedList)
+linkedList.search(-1) // 1 2 3 4
+console.log(linkedList)
