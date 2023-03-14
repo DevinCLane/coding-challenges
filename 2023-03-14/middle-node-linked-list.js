f05bf9e911b2b84a9121e5d9e1f54bb0680eed2e
Devin Lane <devin.c.lane@gmail.com>
2023-03-14 09:14:15 -0700

    middle node of linked list

---
+++ 2023-03-14/middle-node-linked-list.js
@@ -0,0 +1,30 @@
+/* 
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+ 
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+https://leetcode.com/problems/middle-of-the-linked-list/
+*/
+
+// set up a fast and slow variable
+// fast goes twice as fast as slow
+// when fast reaches the end, slow will be in the middle
+
+const middleNode = (head) => {
+    let fast = (slow = head);
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
+};
