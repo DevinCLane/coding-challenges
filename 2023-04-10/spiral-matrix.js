36907bc3a450a27fe51124bb2b296da2f36fa174
Devin Lane <devin.c.lane@gmail.com>
2023-04-11 08:27:02 -0700

    spiral matrix 2

--- 2023-04-10/spiral-matrix.js
+++ 2023-04-10/spiral-matrix.js
@@ -20,7 +20,37 @@ Constraints:
 
 */
 
-const spiralMatrix = (matrix) => {};
+// incomplete, doesn't work. getting there though.
+const spiralMatrix = (matrix) => {
+    let result = [];
+    let left = matrix[0][0];
+    let right = matrix[0].length - 1;
+    let top = matrix[0];
+    let bottom = matrix.length - 1;
+    // let size = size of the array
+    while (result.length < size) {
+        for (let i = left; i <= right && result.length < size; i++) {
+            result.push(matrix[0][i]);
+        }
+        top++;
+
+        for (let i = right; i >= bottom && result.length < size; i++) {
+            result.push(matrix[i]);
+        }
+        right--;
+
+        for (let i = bottom; i >= left && result.length < size; i--) {
+            result.push(bottom);
+        }
+        bottom++;
+
+        for (let i = bottom; i >= top && result.length < size; i--) {
+            result.push(left);
+        }
+        left++;
+    }
+    return result;
+};
 
 console.log(
     spiralMatrix([
