b0adbb556a6e6fb4a15da232026b59b6fb557976
Devin Lane <devin.c.lane@gmail.com>
2023-11-01 10:17:13 -0700

    sum mixed array

---
+++ 2023-11-01/sum-mixed-array.js
@@ -0,0 +1,16 @@
+/* 
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+Return your answer as a number.
+
+console.log(sumMix([9, 3, '7', '3']), 22);
+console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42);
+console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41);
+*/
+
+const sumMixedArray = (arr) => {
+    return arr.reduce((acc, curr) => acc + +curr, 0);
+};
+
+console.log(sumMixedArray([9, 3, "7", "3"]), 22);
+console.log(sumMixedArray(["5", "0", 9, 3, 2, 1, "9", 6, 7]), 42);
+console.log(sumMixedArray(["3", 6, 6, 0, "5", 8, 5, "6", 2, "0"]), 41);
