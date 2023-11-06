9466a17784fe89c798652dcf1777920d0807be52
Devin Lane <devin.c.lane@gmail.com>
2023-11-06 10:48:43 -0800

    even numbers in array

---
+++ 2023-11-06/even-numbers-in-array.js
@@ -0,0 +1,25 @@
+/* 
+Even numbers in an array
+
+Given an array of digital numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers. For example:
+([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
+([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
+([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+
+https://www.codewars.com/kata/5a431c0de1ce0ec33a00000c/solutions/javascript
+*/
+
+const evenNumbers = (arr, number) => {
+    // filter by even numbers
+    // slice returns a shallow copy of the array from start to end points given.
+    return arr.filter((n) => n % 2 === 0).slice(-number);
+};
+
+Array.prototype.splice();
+
+console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3), [4, 6, 8]);
+console.log(
+    evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2),
+    [-8, 26]
+);
+console.log(evenNumbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1), [6]);
