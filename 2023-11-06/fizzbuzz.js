afa534e2c51af59e150a4485e007e9bf67d77bdc
Devin Lane <devin.c.lane@gmail.com>
2023-11-06 10:37:34 -0800

    fizzbuzz

---
+++ 2023-11-06/fizzbuzz.js
@@ -0,0 +1,25 @@
+/* 
+
+Fizzbuzz
+
+Given a number as an input, print out every integer from 1 to that number. However, when the integer is divisible by 3, print out “Fizz”; when it’s divisible by 5, print out “Buzz”; when it’s divisible by both 3 and 5, print out “Fizz Buzz”.
+*/
+
+const fizzbuzz = (n) => {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("fizzbuzz");
+        } else if (i % 3 === 0) {
+            console.log("fizz");
+        } else if (i % 5 === 0) {
+            console.log("buzz");
+        } else {
+            console.log(i);
+        }
+    }
+};
+
+console.log(
+    fizzbuzz(15),
+    "1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzbuzz"
+);
