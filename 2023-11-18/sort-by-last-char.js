9c2c60ce0a4fd0088002de2a4ee938b53b198860
Devin Lane <devin.c.lane@gmail.com>
2023-11-18 09:44:19 -0800

    sort by last char

---
+++ 2023-11-18/sort-by-last-char.js
@@ -0,0 +1,53 @@
+/* 
+Sort by last char
+
+Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+If two words have the same last letter, the returned array should show them in the order they appeared in the given string.
+
+All inputs will be valid.
+
+e.g.,
+last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']
+last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']
+last('take me to semynak'), ['take', 'me', 'semynak', 'to'] 
+
+https://www.codewars.com/kata/57eba158e8ca2c8aba0002a0
+*/
+
+const sortByLastChar = (str) => {
+    // split into array
+    // sort by last char using charCodeAt
+    return str
+        .split(" ")
+        .sort(
+            (a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1)
+        );
+};
+
+console.log(sortByLastChar("man i need a taxi up to ubud"), [
+    "a",
+    "need",
+    "ubud",
+    "i",
+    "taxi",
+    "man",
+    "to",
+    "up",
+]);
+console.log(sortByLastChar("what time are we climbing up the volcano"), [
+    "time",
+    "are",
+    "we",
+    "the",
+    "climbing",
+    "volcano",
+    "up",
+    "what",
+]);
+console.log(sortByLastChar("take me to semynak"), [
+    "take",
+    "me",
+    "semynak",
+    "to",
+]);
