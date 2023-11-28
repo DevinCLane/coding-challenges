960321d6fe9267c32e9eab26f566e35e85aec695
Devin Lane <devin.c.lane@gmail.com>
2023-11-28 08:57:32 -0800

    average length repeat

---
+++ 2023-11-18/average-length-repeat.js
@@ -0,0 +1,30 @@
+/* 
+Average length repeat
+
+// Given an array of strings of the same letter type. Return a new array, which will differ in that the length of each element is equal to the average length of the elements of the previous array.
+
+// A few examples:
+
+// ['u', 'y'] =>  ['u', 'y'] // average length is 1
+// ['aa', 'bbb', 'cccc'] => ['aaa', 'bbb', 'ccc'] // average length is 3
+// ['aa', 'bb', 'ddd', 'eee'] => ['aaa', 'bbb', 'ddd', 'eee'] // average length is 2.5 round up to 3
+// If the average length is not an integer, use Math.round().
+// The input array's length > 1
+*/
+
+const averageLengthRepeat = (arr) => {
+    // get the average length of each element
+    // repeat each string by that amount
+    // also could use arr.reduce((acc, curr) => acc + curr.length, 0)
+    const averageLength = Math.round(arr.join("").length / arr.length);
+    return arr.map((value) => value[0].repeat(averageLength));
+};
+
+console.log(averageLengthRepeat(["u", "y"]), ["u", "y"]); // average length is 1
+console.log(averageLengthRepeat(["aa", "bbb", "cccc"]), ["aaa", "bbb", "ccc"]); // average length is 3
+console.log(averageLengthRepeat(["aa", "bb", "ddd", "eee"]), [
+    "aaa",
+    "bbb",
+    "ddd",
+    "eee",
+]);
