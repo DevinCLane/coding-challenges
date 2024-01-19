4d3a2ad987d97ce4c11ee0964e29b9a36f038282
Devin Lane <devin.c.lane@gmail.com>
2024-01-19 09:38:50 -0800

    greatest common divisor

---
+++ 2024-01-19/greatest-common-divisor.js
@@ -0,0 +1,31 @@
+/* 
+Greatest Common Divisor
+Find the greatest common divisor of two positive integers.
+The integers can be large, so you need to find a clever solution.
+The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+console.log(greatestCommonDivisor(30,12),6)
+console.log(greatestCommonDivisor(8,9),1)
+console.log(greatestCommonDivisor(1,1),1)
+*/
+
+// recursive using the Euclidean algorithm
+const greatestCommonDivisor = (x, y) => {
+    return y === 0 ? x : greatestCommonDivisor(y, x % y);
+};
+
+console.log(greatestCommonDivisor(30, 12), 6);
+console.log(greatestCommonDivisor(8, 9), 1);
+console.log(greatestCommonDivisor(1, 1), 1);
+
+const greatestCommonDivisor2 = (x, y) => {
+    for (let i = Math.min(x, y); i > 0; i--) {
+        if (y % i === 0 && x % i === 0) {
+            return i;
+        }
+    }
+};
+
+console.log(greatestCommonDivisor2(30, 12), 6);
+console.log(greatestCommonDivisor2(8, 9), 1);
+console.log(greatestCommonDivisor2(1, 1), 1);
