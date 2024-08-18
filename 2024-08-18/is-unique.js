9fe50f093560641e014d80188e70abb4d52d55e2
Devin Lane <devin.c.lane@gmail.com>
2024-08-18 09:56:07 -0700

    check if array is unique

---
+++ 2024-08-18/is-unique.js
@@ -0,0 +1,20 @@
+/* 
+
+check if an array has all unique items
+*/
+
+const isUnique = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (i !== j && arr[i] === arr[j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
+
+console.log(isUnique([1, 2, 3, 4, 5]), true);
+console.log(isUnique([1, 2, 3, 4, 5, 5]), false);
+console.log(isUnique([1, 2, 3, 4, 5, 10, -1]), true);
+console.log(isUnique([1, 2, 3, 4, 5, 5, 10, -1, 2]), false);
