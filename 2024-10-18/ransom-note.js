f19bcf6ebd3018f7ce231ac2c016359d980fd322
Devin Lane <devin.c.lane@gmail.com>
2024-10-18 10:49:24 -0700

    ransom note

---
+++ 2024-10-18/ransom-note.js
@@ -0,0 +1,64 @@
+/* 
+Ransom note
+
+// Given a magazine of words and a ransom note, determine if it’s possible to “cut out” and create the ransom note from the magazine words.
+
+const magazine =
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+
+console.log(ransomNote("sit ad est sint", magazine), true)
+console.log(ransomNote("sit ad est love", magazine), false)
+console.log(ransomNote("sit ad est sint in in", magazine), true)
+console.log(ransomNote("sit ad est sint in in in in", magazine), false)
+*/
+
+const magazine =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+
+const removePunctuation = (word) => {
+    return word
+        .split("")
+        .filter((char) => char !== "." && char !== ",")
+        .join("");
+};
+
+/**
+ *
+ * @param {string} note - the phrase we are trying to create
+ * @param {string} magazine - the magazine from which we create the phrase
+ * @returns {boolean} - true/false if we can create the phrase from the magazine
+ */
+const ransomNote = (note, magazine) => {
+    // create hashmap counting occurances of the words in the magazine
+    // loop over the word array
+    // if no cases are found, then it's true
+    // if the word exists in the array
+    // reduce the count by one
+    // if the count goes below 0, then it's false
+    // if the word doesn't exist in the array it's false
+    const magazineMap = {};
+    const magazineArr = magazine
+        .toLowerCase()
+        .split(" ")
+        .map(removePunctuation);
+    const noteArr = note.toLowerCase().split(" ");
+
+    for (const word of magazineArr) {
+        magazineMap[word] = ++magazineMap[word] || 1;
+    }
+
+    for (const word of noteArr) {
+        if (magazineMap[word]) {
+            magazineMap[word]--;
+            if (magazineMap[word] < 0) {
+                return false;
+            }
+        } else return false;
+    }
+    return true;
+};
+
+console.log(ransomNote("sit ad est sint", magazine), true);
+console.log(ransomNote("sit ad est love", magazine), false);
+console.log(ransomNote("sit ad est sint in in", magazine), true);
+console.log(ransomNote("sit ad est sint in in in in", magazine), false);
