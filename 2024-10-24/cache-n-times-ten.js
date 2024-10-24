919962690ebaeadbe68b0ac2436e8eaf673f43c6
Devin Lane <devin.c.lane@gmail.com>
2024-10-24 11:18:57 -0700

    cache n times 10

---
+++ 2024-10-24/cache-n-times-ten.js
@@ -0,0 +1,34 @@
+/* 
+write a function that multiplies a number n times 10
+
+then use an object to cache the results of the n times 10 function
+
+then write a function that multiplies a number n times 10, but first checks if the result is already in the cache.
+
+console.log("~~~task 1~~~");
+console.log("times10 returns: ", times10(9));
+
+console.log("~~~task 2~~~");
+console.log("task 2 calculated value: ", memoTimes10(9)); // calculated
+console.log("task 2 cached value: ", memoTimes10(9)); // cached
+*/
+
+const times10 = (n) => n * 10;
+
+const cache = {};
+const memoTimes10 = (n) => {
+    if (n in cache) {
+        return cache[n];
+    } else {
+        let result = times10(n);
+        cache[n] = result;
+        return result;
+    }
+};
+
+console.log("~~~task 1~~~");
+console.log("times10 returns: ", times10(9));
+
+console.log("~~~task 2~~~");
+console.log("task 2 calculated value: ", memoTimes10(9)); // calculated
+console.log("task 2 cached value: ", memoTimes10(9)); // cached
