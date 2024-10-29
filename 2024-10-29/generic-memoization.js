7f284b5cf5fbe19d4ed816d5f6ba2f6f6fab668d
Devin Lane <devin.c.lane@gmail.com>
2024-10-29 10:33:31 -0700

    generic memoization

---
+++ 2024-10-29/generic-memoization.js
@@ -0,0 +1,28 @@
+/* 
+Make your memo function generic and accept the times10 function as a callback rather than defining
+the n * 10 logic inside the if/else or pulling it in from the global scope
+
+protip: take advantage of the fact that parameters are saved in the closure as well,
+just like the cache from the previous example
+*/
+
+const times10 = (n) => n * 10;
+
+const genericMemo = (cb) => {
+    const cache = {};
+    return (n) => {
+        if (n in cache) {
+            console.log("fetching from cache...");
+            return cache[n];
+        } else {
+            console.log("calculating result...");
+            let result = cb(n);
+            cache[n] = result;
+            return result;
+        }
+    };
+};
+
+const times10Memo = genericMemo(times10);
+console.log(times10Memo(9));
+console.log(times10Memo(9));
