a94c0eaf0e6c39a6cbde751bace8b6038366c346
Devin Lane <devin.c.lane@gmail.com>
2024-11-05 10:47:13 -0800

    join elements recursively

---
+++ 2024-11-05/join-elements.js
@@ -0,0 +1,24 @@
+/* 
+
+Using recursion, write a function that inserts a given character between each character in an array. Don't add the character at the end.
+
+example: joinElements(["s", "cr", "t cod", " :) :)"], "e") => "secret code :) :)"
+*/
+
+function joinElements(array, joinString) {
+    function recurse(index, resultSoFar) {
+        resultSoFar += array[index];
+
+        if (index === array.length - 1) {
+            return resultSoFar;
+        } else {
+            return recurse(index + 1, resultSoFar + joinString);
+        }
+    }
+    return recurse(0, "");
+}
+
+console.log(
+    joinElements(["s", "cr", "t cod", " :) :)"], "e"),
+    "secret code :) :)"
+);
