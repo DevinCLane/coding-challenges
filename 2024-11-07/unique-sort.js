fcbc63f50dc8c7238579ed85a9d34b4ad92c6073
Devin Lane <devin.c.lane@gmail.com>
2024-11-07 12:28:47 -0800

    unique sort

---
+++ 2024-11-07/unique-sort.js
@@ -0,0 +1,33 @@
+/* 
+Task: Transform this simple sorting algorithm into a unique sort.
+It should not return any duplicate values in the sorted array.
+
+input: [1,5,2,1] => output: [1,2,5]
+input: [4,2,2,3,2,2,2] => output: [2,3,4]
+*/
+
+const uniqueSort = (arr) => {
+    const set = new Set(arr);
+    const uniqueArr = Array.from(set);
+    return uniqueArr.sort((a, b) => a - b);
+};
+
+console.log(uniqueSort([1, 5, 2, 1]), [1, 2, 5]);
+console.log(uniqueSort([4, 2, 2, 3, 2, 2, 2]), [2, 3, 4]);
+
+const uniqueSort2 = (arr) => {
+    const cache = {};
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!cache[arr[i]]) {
+            result.push(arr[i]);
+            cache[arr[i]] = true;
+        }
+    }
+
+    return result.sort((a, b) => a - b);
+};
+
+console.log(uniqueSort2([1, 5, 2, 1]), [1, 2, 5]);
+console.log(uniqueSort2([4, 2, 2, 3, 2, 2, 2]), [2, 3, 4]);
