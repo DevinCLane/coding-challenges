cdf015eea5bf108576e6864d2201d8d0b17c28a1
Devin Lane <devin.c.lane@gmail.com>
2024-12-06 12:30:26 -0800

    loop to recursion

---
+++ 2024-12-06/loop-to-recursion.js
@@ -0,0 +1,29 @@
+/*
+Rewrite this function so that it uses a loop rather than recursion
+*/
+
+function joinElements(array, joinString) {
+    function recurse(index, resultSoFar) {
+        resultSoFar += array[index];
+
+        if (index === array.length - 1) {
+            return resultSoFar;
+        } else {
+            return recurse(index + 1, resultSoFar + joinString);
+        }
+    }
+
+    return recurse(0, "");
+}
+
+console.log(joinElements(["s", "cr", "t cod", " :) :)"], "e"));
+
+function joinElementsLoop(arr, joinString) {
+    let result = "";
+    for (let i = 0; i < arr.length - 1; i++) {
+        result += arr[i] + joinString;
+    }
+    return result + arr[arr.length - 1];
+}
+
+console.log(joinElementsLoop(["s", "cr", "t cod", " :) :)"], "e"));
