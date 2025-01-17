10d0dbee07af9aee3f012fd6e7d9683c3bf06f16
Devin Lane <devin.c.lane@gmail.com>
2025-01-17 10:38:13 -0800

    bubble sort

---
+++ 2025-01-17/bubble-sort.js
@@ -0,0 +1,31 @@
+/* 
+Implement basic bubble sort
+
+[]
+
+*/
+
+const arrayRandom = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+const arrayOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arrayReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+function swap(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 1; j < arr.length; j++) {
+            if (arr[j - 1] > arr[j]) {
+                swap(arr, j - 1, j);
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort(arrayRandom));
+console.log(bubbleSort(arrayOrdered));
+console.log(bubbleSort(arrayReversed));
