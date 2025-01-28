ea6334955824cf6e1fdf48502d61f62bd41798fe
Devin Lane <devin.c.lane@gmail.com>
2025-01-28 09:15:30 -0800

    factorial memoized

---
+++ factorial-memoized.js
@@ -0,0 +1,32 @@
+/* 
+
+task 1: write your own recursive factorial method
+
+task 2: create a memo function to memoize the factorial method
+
+(the memo is only helping us if we need to call the function again)
+*/
+
+const memo = (cb) => {
+    const cache = {};
+    return (n) => {
+        if (n in cache) {
+            return cache[n];
+        } else {
+            let result = cb(n);
+            cache[n] = result;
+            return result;
+        }
+    };
+};
+
+const factorial = (n) => {
+    if (n === 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+};
+
+const memoizedFactorial = memo(factorial);
+console.log(memoizedFactorial(5), 120);
