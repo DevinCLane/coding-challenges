d3f91c8f71e3ec735ccfdcb6701dbca8d91d2724
Devin Lane <devin.c.lane@gmail.com>
2025-06-05 09:36:03 -0700

    hash set

---
+++ 2025-06-05/hashset.js
@@ -0,0 +1,72 @@
+/* 
+
+9. 
+705. Design HashSet
+Design a HashSet without using any built-in hash table libraries.
+Implement MyHashSet class:
+void add(key) Inserts the value key into the HashSet.
+bool contains(key) Returns whether the value key exists in the HashSet or not.
+void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+ 
+Example 1:
+Input
+["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+[[], [1], [2], [1], [3], [2], [2], [2], [2]]
+Output
+[null, null, null, true, false, null, true, null, false]
+
+Explanation
+MyHashSet myHashSet = new MyHashSet();
+myHashSet.add(1);      // set = [1]
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(1); // return True
+myHashSet.contains(3); // return False, (not found)
+myHashSet.add(2);      // set = [1, 2]
+myHashSet.contains(2); // return True
+myHashSet.remove(2);   // set = [1]
+myHashSet.contains(2); // return False, (already removed)
+ 
+Constraints:
+0 <= key <= 106
+At most 104 calls will be made to add, remove, and contains.
+*/
+
+// key info: a hashset is like a set in that it doesn't contain duplicates
+
+/**
+ * time: O(n) for each function call
+ * space: O(n)
+ */
+class HashSet {
+    constructor() {
+        this.storage = [];
+    }
+    /**
+     *
+     * @param {number} key
+     * @return {void}
+     */
+    add(key) {
+        if (!this.storage.includes(key)) {
+            this.storage.push(key);
+        }
+    }
+
+    /**
+     *
+     * @param {number} key
+     * @returns {void}
+     */
+    remove(key) {
+        this.storage.splice(this.storage.indexOf(key, 1));
+    }
+
+    /**
+     *
+     * @param {number} key
+     * @returns {boolean}
+     */
+    contains(key) {
+        return this.storage.includes(key);
+    }
+}
