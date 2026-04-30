537d2a72de870f2eda6422aaa6f7c545ee916941
Devin Lane <devin.c.lane@gmail.com>
2026-04-29 17:08:52 -0700

    hash map

---
+++ 2026/2026-04-29/hashmap.js
@@ -0,0 +1,103 @@
+/* 
+Design HashMap
+
+Design a HashMap without using any built-in hash table libraries.
+Implement the MyHashMap class:
+MyHashMap() initializes the object with an empty map.
+void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
+Example 1:
+Input: ["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+[[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+
+Output: [null, null, null, 1, -1, null, 1, null, -1]
+Explanation:
+MyHashMap myHashMap = new MyHashMap();
+myHashMap.put(1, 1); // The map is now [[1,1]]
+myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+myHashMap.get(1); // return 1, The map is now [[1,1], [2,2]]
+myHashMap.get(3); // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+myHashMap.get(2); // return 1, The map is now [[1,1], [2,1]]
+myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+myHashMap.get(2); // return -1 (i.e., not found), The map is now [[1,1]]
+Constraints:
+0 <= key, value <= 1,000,000
+At most 10,000 calls will be made to put, get, and remove.
+*/
+
+class HashMapArr {
+    constructor() {
+        this.map = new Array(1000001).fill(-1);
+    }
+
+    put(key, value) {
+        this.map[key] = value;
+    }
+
+    get(key) {
+        return this.map[key];
+    }
+
+    remove(key) {
+        this.map[key] = -1;
+    }
+}
+
+class ListNode {
+    constructor(key = -1, val = -1, next = null) {
+        this.key = key;
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class HashMap {
+    constructor() {
+        this.map = Array.from({ length: 1000 }, () => new ListNode());
+    }
+
+    hash(key) {
+        return key % this.map.length;
+    }
+
+    put(key, val) {
+        let curr = this.map[this.hash(key)];
+        while (curr.next) {
+            if (curr.next.key === key) {
+                curr.next.val = val;
+                return;
+            }
+            curr = curr.next;
+        }
+        curr.next = new ListNode(key, val);
+    }
+
+    get(key, val) {
+        let curr = this.map[this.hash(key)];
+        while (curr.next) {
+            if (curr.next.key === key) {
+                return curr.next.val;
+            }
+            curr = curr.next;
+        }
+        return -1;
+    }
+
+    remove(key, val) {
+        let curr = this.map[this.hash(key)];
+        while (curr.next) {
+            if (curr.next.key === key) {
+                curr.next = curr.next.next;
+                return;
+            }
+            curr = curr.next;
+        }
+    }
+}
+
+const hashmap = new HashMap();
+hashmap.put(1, 1);
+hashmap.put(2, 2);
+console.log(hashmap);
